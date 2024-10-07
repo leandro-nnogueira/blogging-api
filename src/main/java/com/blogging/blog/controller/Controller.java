eb049fd090dbd63a2a70102d67d5f5af76edcd23
Leandro Nogueira <LeandroNascimentoNogueira@gmail.com>
2024-10-06 21:29:42 -0300

    Refactor .gitignore and add application.properties

--- src/main/java/com/blogging/blog/controller/Controller.java
+++ src/main/java/com/blogging/blog/controller/Controller.java
@@ -3,6 +3,8 @@ package com.blogging.blog.controller;
 import com.blogging.blog.model.Post;
 import com.blogging.blog.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +18,46 @@ public class Controller {
 
     // List all posts
     @GetMapping
-    public List<Post> findAll() {
-        return service.findAll();
+    public ResponseEntity<List<Post>> findAll() {
+        List<Post> posts = service.findAll();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 
     // Find post by id
     @GetMapping("/{id}")
-    public Post findById(@PathVariable Long id) {
-        return service.findById(id);
+    public ResponseEntity<Post> findById(@PathVariable Long id) {
+        Post post = service.findById(id);
+        if (post != null) {
+            return new ResponseEntity<>(post, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    // Save post
-    @PostMapping
-    public Post save(@RequestBody Post post) {
-        return service.save(post);
+    // Save a post
+    @PostMapping("/add")
+    public ResponseEntity<Post> addUser(@RequestBody Post post) {
+        Post newPost = service.save(post);
+        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
     }
 
-    // Update post
     @PutMapping("/{id}")
-    public Post update(@RequestBody Post post) {
-        return service.update(post);
+    public ResponseEntity<Post> update(@PathVariable Long id, @RequestBody Post post) {
+        Post existingPost = service.findById(id);
+        if (existingPost != null) {
+            post.setId(id);
+            Post updatedPost = service.update(post);
+            return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     // Delete post
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
